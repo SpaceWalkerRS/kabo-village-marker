e14721970b88d5ba03ff8c73501b2d4bcfba4fcc
Space Walker <spacedoesrs@gmail.com>
2025-10-20 14:50:22 +0200

    remove cooldown for sending updates

--- src/main/java/kabopc/village_marker/KaboVillageMarker.java
+++ src/main/java/kabopc/village_marker/KaboVillageMarker.java
@@ -16,7 +16,6 @@ public class KaboVillageMarker {
 	private final SavedVillageData villageData;
 
 	private boolean dirty;
-	private int cooldown;
 
 	public KaboVillageMarker(ServerWorld world, SavedVillageData villageData) {
 		this.world = world;
@@ -28,14 +27,13 @@ public class KaboVillageMarker {
 	}
 
 	public void tick() {
-		if (dirty && cooldown-- == 0) {
+		if (dirty) {
 			// TODO: the OG mod breaks all the data up into small parts
 			// apparently packets were getting too big???
 			// check if we need to do that...
 			ServerPlayNetworking.send(world.dimension.getType(), KaboVillageMarkerMod.DATA_CHANNEL, this::write);
 
 			dirty = false;
-			cooldown = 400;
 		}
 	}
 
